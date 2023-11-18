0b11a5b788291c356440f0842319ecc6ea564192
dbergs72 <dbergs72@gmail.com>
2023-11-18 14:59:40 -0500

    Add basic router and navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./home/index";
+import Profile from "./profile";
+import Search from "./search";
+import NavBar from "./NavBar/index.js";
 
 function App() {
+  const screen = "labs";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <HashRouter>
+        <div>
+            <NavBar/>
+            <div style={{marginTop: 56}}>
+              <Routes>
+                <Route path="/" element={<Navigate to="/Home" />} />
+                <Route path="/Home" element={<Home />} />
+                  <Route path="/Profile" element={<Profile />} />
+                  <Route path="/Search" element={<Search />} />
+              </Routes>
+            </div>
+        </div>
+      </HashRouter>
   );
 }
 
