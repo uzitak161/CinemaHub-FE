7d87f18567eee366a3238b253d45ce48a0308db2
uzitak161 <77938678+uzitak161@users.noreply.github.com>
2023-11-19 23:02:28 -0500

    Added movie reviews under profile

--- src/profile/index.js
+++ src/profile/index.js
@@ -5,16 +5,50 @@ import './index.css';
 import { Link } from "react-router-dom";
 
 
+function generateAllUserReviews(reviews) {
+    return (<div>
+        {reviews.map((review) => {
+            return generateReviewCard(review);
+        })}
+    </div>
+
+    )
+
+}
+
+
+
+// We can give this a review object and it will render the review
+function generateReviewCard(review) {
+
+
+    return (
+        <div className="card">
+            <div className="card-body">
+                <h5 className="card-title">{review.movieTitle}</h5>
+                <h6 className="card-subtitle mb-2 text-muted">
+                    {Array.from({ length: review.rating }, (_, index) => (
+                        <span className="stars" key={index}>★</span>
+                    ))}
+                </h6>
+                <p className="card-text">{review.review}</p>
+            </div>
+        </div>
+    )
+}
+
+
 function Profile() {
 
     // This is temporary, will eventually use redux to store login status
     const [loggedIn, setStatus] = useState(true)
+    const [reviews, setReviews] = useState([{ movieTitle: "Pulp Fiction", rating: 5, review: "This movie was great!" }, { movieTitle: "Pulp Fiction", rating: 5, review: "This movie was great!" }])
 
     // Lookup the users profile image if they have one (optional)
     const image = undefined;
 
     const followers = 10;
-    const reviews = 20;
+    const reviewCount = 20;
     const following = 30;
 
 
@@ -31,7 +65,7 @@ function Profile() {
 
                         <div className="d-flex flex-column mx-2 stats">
                             <span className="">Reviews</span>
-                            <span className="number">{reviews}</span>
+                            <span className="number">{reviewCount}</span>
                         </div>
 
                         <div className="d-flex flex-column mx-2 stats">
@@ -42,6 +76,9 @@ function Profile() {
                     <input type="text" className="form-control my-2" placeholder="E-Mail" />
                     <textarea rows="4" className="form-control my-2" cols="50" placeholder="Enter your bio here..."></textarea>
                 </div>
+
+                <h2> Your Reviews </h2>
+                {generateAllUserReviews(reviews)}
             </div>
         )
     } else {
