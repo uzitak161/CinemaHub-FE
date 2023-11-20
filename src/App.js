39cc054a0d003d11e193f9ef85a1b26c311679ed
uzitak161 <77938678+uzitak161@users.noreply.github.com>
2023-11-19 22:07:01 -0500

    Initial login page

--- src/App.js
+++ src/App.js
@@ -3,23 +3,25 @@ import Home from "./home/index";
 import Profile from "./profile";
 import Search from "./search";
 import NavBar from "./NavBar/index.js";
+import Login from "./login/index.js";
 
 function App() {
   const screen = "labs";
   return (
-      <HashRouter>
-        <div>
-            <NavBar/>
-            <div style={{marginTop: 56}}>
-              <Routes>
-                <Route path="/" element={<Navigate to="/Home" />} />
-                <Route path="/Home" element={<Home />} />
-                  <Route path="/Profile" element={<Profile />} />
-                  <Route path="/Search" element={<Search />} />
-              </Routes>
-            </div>
+    <HashRouter>
+      <div>
+        <NavBar />
+        <div style={{ marginTop: 56 }}>
+          <Routes>
+            <Route path="/" element={<Navigate to="/Home" />} />
+            <Route path="/Login" element={<Login />} />
+            <Route path="/Home" element={<Home />} />
+            <Route path="/Profile" element={<Profile />} />
+            <Route path="/Search" element={<Search />} />
+          </Routes>
         </div>
-      </HashRouter>
+      </div>
+    </HashRouter>
   );
 }
 
--- src/NavBar/index.js
+++ src/NavBar/index.js
@@ -1,24 +1,36 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import {useLocation } from "react-router-dom";
 
 function NavBar() {
-    return (
-        <nav className="navbar navbar-expand-sm navbar-dark bg-dark fixed-top">
-            <a className="ms-2 navbar-brand" href="#">CinemaHub</a>
-            <div className="collapse navbar-collapse">
-                <ul className="navbar-nav mr-auto">
-                    <li className="nav-item active">
-                        <Link key={"home"} to={`/home`} className="nav-link">Home</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link key={"profile"} to={`/profile`} className={"nav-link"}>Profile</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link key={"search"} to={`/search`} className="nav-link">Search</Link>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    );
+
+    const pathname = useLocation()
+    if (pathname.pathname.includes("/login")) {
+        return null;
+    } else {
+
+        return (
+            <nav className="navbar navbar-expand-sm navbar-dark bg-dark fixed-top">
+                <a className="ms-2 navbar-brand" href="#">CinemaHub</a>
+                <div className="collapse navbar-collapse">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <Link key={"home"} to={`/home`} className="nav-link">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link key={"profile"} to={`/profile`} className={"nav-link"}>Profile</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link key={"search"} to={`/search`} className="nav-link">Search</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link key={"login"} to={`/login`} className="nav-link">login</Link>
+                        </li>
+
+                    </ul>
+                </div>
+            </nav>
+        );
+    }
 }
 
 export default NavBar;
