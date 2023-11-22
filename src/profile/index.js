62d41b3494fb6761b0209c2b33063db1bbc78cc2
Uzay Takil <uzaytakil01@gmail.com>
2023-11-21 20:53:51 -0500

    Fixed centering

--- src/profile/index.js
+++ src/profile/index.js
@@ -107,7 +107,7 @@ function Profile() {
     if (loggedIn) {
         return (
             <div className='d-flex bd-highlight'>
-                <div className='p-2 flex-fill bd-highlight'>
+                <div className='p-2  bd-highlight'>
                     <div className='row'>
                         <div className='col'>
                             <div className='bio-area form-group'>
