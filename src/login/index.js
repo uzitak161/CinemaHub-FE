9d009687a052f09716c45fb1dd9c6f6083437740
Uzay Takil <uzaytakil01@gmail.com>
2023-11-25 12:39:18 -0500

    Fixed grey body issue

--- src/login/index.js
+++ src/login/index.js
@@ -22,7 +22,7 @@ const Login = () => {
     };
 
     return (
-        <div className="container mt-5">
+        <div className="container-fluid mt-5 main-login">
             <div className="row justify-content-center">
                 <div className="col-md-6">
                     <div className="card">
