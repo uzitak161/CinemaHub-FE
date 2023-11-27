e854563e38c419462110820759cd9fa1407ed13e
uzitak161 <77938678+uzitak161@users.noreply.github.com>
2023-11-27 18:00:44 -0500

    Added navbar back in login page

--- src/NavBar/index.js
+++ src/NavBar/index.js
@@ -1,35 +1,32 @@
 import { Link } from "react-router-dom";
-import {useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 function NavBar() {
 
     const pathname = useLocation()
-    if (pathname.pathname.includes("/login")) {
-        return null;
-    } else {
-        return (
-            <nav className="navbar navbar-expand-sm navbar-dark bg-dark fixed-top">
-                <Link key={"home"} to={`/home`} className="ms-2 navbar-brand">CinemaHub</Link>
-                <div className="collapse navbar-collapse">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item active">
-                            <Link key={"home"} to={`/home`} className="nav-link">Home</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link key={"profile"} to={`/profile`} className={"nav-link"}>Profile</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link key={"search"} to={`/search`} className="nav-link">Search</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link key={"login"} to={`/login`} className="nav-link">Login</Link>
-                        </li>
+    return (
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark fixed-top">
+            <Link key={"home"} to={`/home`} className="ms-2 navbar-brand">CinemaHub</Link>
+            <div className="collapse navbar-collapse">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active">
+                        <Link key={"home"} to={`/home`} className="nav-link">Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link key={"profile"} to={`/profile`} className={"nav-link"}>Profile</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link key={"search"} to={`/search`} className="nav-link">Search</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link key={"login"} to={`/login`} className="nav-link">Login</Link>
+                    </li>
 
-                    </ul>
-                </div>
-            </nav>
-        );
-    }
+                </ul>
+            </div>
+        </nav>
+    );
 }
+
 
 export default NavBar;
