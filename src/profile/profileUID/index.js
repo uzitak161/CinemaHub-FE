9dba33d4aa8b4f62ab4b383231a929497b94ad81
uzitak161 <77938678+uzitak161@users.noreply.github.com>
2023-11-28 15:41:32 -0500

    Updates to profile/UID

--- src/profile/profileUID/index.js
+++ src/profile/profileUID/index.js
@@ -1,10 +1,50 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { FaUserAlt } from 'react-icons/fa';
 import '../index.css';
 import Modal from "react-modal";
 import StatModal from '../statModal';
+import axios from 'axios';
+
+
+
+function generateAllUserReviews(reviews) {
+    return (<div>
+        {reviews.map((review) => {
+            return generateReviewCard(review);
+        })}
+    </div>
+
+    )
+}
+
+// We can give this a review object and it will render the review
+function generateReviewCard(review) {
+
+    const dateOptions = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', timeZoneName: 'short' };
+    const formattedDate = new Date(review.createdAt).toLocaleString('en-US', dateOptions);
+
+
+    return (
+
+        <div className="card">
+            <div className="card-body">
+                <h5 className="card-title">{review.movieId.title}</h5>
+                <h6 className="card-subtitle mb-2 text-muted">
+                    {Array.from({ length: review.starRating }, (_, index) => (
+                        <span className="stars" key={index}>★</span>
+                    ))}
+                </h6>
+                <p className="card-text">{review.text}</p>
+                <p className='float-end mb-0'>Created At: {formattedDate}</p>
+            </div>
+        </div>
+
+    );
+}
 
 function ProfileSpecific() {
+
+    const user = {username: "Specific User", followers: 10, following: 20, reviews: 30, image: undefined}
 
     // This is temporary, will eventually use redux to store login status
     const [loggedIn, setStatus] = useState(true)
@@ -40,46 +80,55 @@ function ProfileSpecific() {
         }
     }
 
+    const getReviews = async () => {
+        const URL = 'http://localhost:4000/api/comments'
+        const response = await axios.get(URL);
+        setReviews(response.data)
+    }
+
+    useEffect(() => {
+        getReviews();
+    }, [])
 
     return (
         <div className='row'>
-                        <div className='col'>
-                            <div className='bio-area form-group'>
-                                {image ? image : <FaUserAlt className='avatar' />}
-                                <div onClick={() => setModalOpen(true)} className="p-2 mt-2 bg-primary d-flex justify-content-between rounded text-white stats">
-                                    <div className="d-flex flex-column mx-2 stats">
-                                        <span className="">Followers</span>
-                                        <span className="number">{followers}</span>
-                                    </div>
-
-                                    <div className="d-flex flex-column mx-2 stats">
-                                        <span className="">Reviews</span>
-                                        <span className="number">{reviewCount}</span>
-                                    </div>
-
-                                    <div className="d-flex flex-column mx-2 stats">
-                                        <span className="">Following</span>
-                                        <span className="number">{following}</span>
-                                    </div>
-                                </div>
-                                <Modal
-                                    isOpen={modalOpen}
-                                    onRequestClose={() => setModalOpen(false)}
-                                    style={modalStyle}
-                                >
-                                    <StatModal setModal={setModalOpen} />
-
-
-                                </Modal>
-                                <input type="text" className="form-control my-2" placeholder="E-Mail" />
-                                <textarea rows="4" className="form-control my-2" cols="50" placeholder="Enter your bio here..."></textarea>
-                            </div>
+            <div className='col'>
+                <div className='bio-area form-group'>
+                    {image ? image : <FaUserAlt className='avatar nohover' style={{textDecocation: "none"}} />}
+                    <div onClick={() => setModalOpen(true)} className="p-2 mt-2 bg-primary d-flex justify-content-between rounded text-white stats">
+                        <div className="d-flex flex-column mx-2 stats">
+                            <span className="">Followers</span>
+                            <span className="number">{followers}</span>
                         </div>
-                        <div className='col'>
-                            <h2> Other Field </h2>
-                            
+
+                        <div className="d-flex flex-column mx-2 stats">
+                            <span className="">Reviews</span>
+                            <span className="number">{reviewCount}</span>
+                        </div>
+
+                        <div className="d-flex flex-column mx-2 stats">
+                            <span className="">Following</span>
+                            <span className="number">{following}</span>
                         </div>
                     </div>
+                    <Modal
+                        isOpen={modalOpen}
+                        onRequestClose={() => setModalOpen(false)}
+                        style={modalStyle}
+                    >
+                        <StatModal setModal={setModalOpen} />
+
+
+                    </Modal>
+                    <h3 className='mt-2'>{user.username}</h3>
+                    <textarea readOnly rows="4" className="form-control my-2" cols="50" placeholder="Enter your bio here..." value="Specific User's bio "></textarea>
+                </div>
+            </div>
+            <div className='col'>
+                <h2> {user.username}'s Reviews </h2>
+                {generateAllUserReviews(getRecent(reviews))}
+            </div>
+        </div>
     )
 }
 
